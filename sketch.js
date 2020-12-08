8e0e6d113c7cfd310bbbcf32b1a5a63ae5765cd0
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2020-12-07 21:38:35 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,17 +13,17 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	roof1 = new Roof(600, 100, 1200, 20);
-	bob1 = new Bob(600, 300, 25);
-	bob2 = new Bob(650, 300, 25);
-	bob3 = new Bob(700, 300, 25);
-	bob4 = new Bob(750, 300, 25);
-	bob5 = new Bob(800, 300, 25);
-	rope1 = new Rope(bob1.body,roof1.body, -100);
-	rope2 = new Rope(bob2.body, roof1.body, -50);
-	rope3 = new Rope( bob3.body, roof1.body, +0);
-	rope4 = new Rope(bob4.body, roof1.body, +50 );
-	rope5 = new Rope( bob5.body, roof1.body, +100);
+  roof1 = new Roof(600, 50, 1200, 20);
+  bob1 = new Bob(300, 300, "royalblue");
+  bob2 = new Bob(360,300, "purple");
+  bob3 = new Bob(420, 300, "royalblue");
+  bob4 = new Bob(480, 300, "purple");
+  bob5 = new Bob(540, 300, "royalblue");
+  rope1 = new Rope(bob1.body, {x:300, y:50}, "purple");
+  rope2 = new Rope(bob2.body,{x:360, y:50}, "royalblue");
+  rope3 = new Rope(bob3.body,{x:420, y:50}, "purple");
+  rope4 = new Rope(bob4.body,{x:480, y:50}, "royalblue");
+  rope5 = new Rope(bob5.body, {x:540, y:50}, "purple");
 	
 	Engine.run(engine);
   
@@ -32,7 +32,9 @@ function draw() {
   rectMode(CENTER);
   background("turquoise");
   Engine.update(engine);
-
+  fill("#910046");
+  textSize(15);
+  text("Drag the mouse on the first bob to relaunch", 275, 370);
   roof1.display();
   bob1.display();
   bob2.display();
@@ -44,7 +46,6 @@ function draw() {
   rope3.display();
   rope4.display();
   rope5.display();
-  
   drawSprites();
  
 }
